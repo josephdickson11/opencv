cc70d6fa09a48ba99efe329ad087bbb7b1656304
Ibeun Joseph <josepholadipupoibeun@gmail.com>
2020-11-15 01:29:14 +0100

    warp perspective

--- chapter5.py
+++ chapter5.py
@@ -1,7 +1,18 @@
 import cv2
+import numpy as np
 
-img = cv2.imread("resources/1591988497278.jpg")
+img = cv2.imread("resources/cards.jpg")
 
-cv2.imshow("angie", img)
+width,height =250, 350
+pts1 = np.float32([[111,219],[287,188],[154,482],[352,440]])
+pts2 = np.float32([[0,0],[width,0],[0,height],[width,height]])
+matrix = cv2.getPerspectiveTransform(pts1,pts2)
+
+img_output = cv2.warpPerspective(img, matrix, (width,height))
+
+
+
+cv2.imshow("card", img)
+cv2.imshow("selected card", img_output)
 
 cv2.waitKey(0)
